4cc89e43b0354daaaf12654fe6e04b4c93cfc538
Luís Sukys <sukys22@gmail.com>
2020-11-05 20:44:11 -0300

    exercicio completo

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var express = require('express');
 var cors = require('cors');
 
-// require and use "multer"...
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -17,6 +18,13 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+
+// {"name":"WhatsApp Image 2020-11-04 at 08.52.15.jpeg","type":"image/jpeg","size":100474}
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res){
+  let data = {name:req.file.originalname, type:req.file.mimetype, size:req.file.size}
+  res.json(data);
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
